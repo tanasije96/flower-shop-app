0cc4a395b0a53984c62951e15aad4874262ef54a
tanasije286 <tanasije286@gmail.com>
2026-05-18 11:54:40 -0400

    Spring boot initial commit

---
+++ backend/backend/src/test/java/com/tanasije/flower_shop/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanasije.flower_shop.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
