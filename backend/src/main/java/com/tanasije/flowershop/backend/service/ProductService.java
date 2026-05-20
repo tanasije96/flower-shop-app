2c4b6af7f5c07cfa295c9c0d611f1a6d1bff8e95
tanasije286 <tanasije286@gmail.com>
2026-05-20 16:08:18 -0400

    Created Product Service

---
+++ backend/src/main/java/com/tanasije/flowershop/backend/service/ProductService.java
@@ -0,0 +1,34 @@
+package com.tanasije.flowershop.backend.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.tanasije.flowershop.backend.dto.ProductDTO;
+import com.tanasije.flowershop.backend.model.Product;
+import com.tanasije.flowershop.backend.repository.ProductRepository;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<ProductDTO> getAllProducts() {
+        List<Product> products = productRepository.findAll();
+
+        return products.stream().map(this::convertToDTO).toList();
+    }
+
+    private ProductDTO convertToDTO(Product product) {
+        ProductDTO dto = new ProductDTO();
+        dto.setId(product.getId());
+        dto.setName(product.getName());
+        dto.setPrice(product.getPrice());
+        dto.setImageUrl(product.getImageUrl());
+        return dto;
+    }
+}
