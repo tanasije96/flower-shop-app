36952f25a9c5c5af2d78d678c6251548246196a2
tanasije286 <tanasije286@gmail.com>
2026-05-20 15:25:31 -0400

    Added product model

---
+++ backend/src/main/java/com/tanasije/flowershop/backend/model/Product.java
@@ -0,0 +1,18 @@
+package com.tanasije.flowershop.backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private double price;
+    private String imageUrl;
+}
