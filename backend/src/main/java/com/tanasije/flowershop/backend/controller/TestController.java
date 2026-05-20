895207b2c69f9fa86181b35623df02e8d850ff27
tanasije286 <tanasije286@gmail.com>
2026-05-20 15:25:18 -0400

    Folder restructure

--- backend/src/main/java/com/tanasije/flowershop/backend/controller/TestController.java
+++ backend/src/main/java/com/tanasije/flowershop/backend/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.tanasije.flower_shop.backend.controllers;
+package com.tanasije.flowershop.backend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
