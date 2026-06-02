517e3ad2203145477e277b50f35ee7628368f46a
tanasije96 <marko.tanasije@gmail.com>
2026-06-02 14:45:22 -0400

    Added isDeletedFalse query to productrepo

--- backend/src/main/java/com/tanasije/flowershop/backend/repository/ProductRepository.java
+++ backend/src/main/java/com/tanasije/flowershop/backend/repository/ProductRepository.java
@@ -1,8 +1,11 @@
 package com.tanasije.flowershop.backend.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.tanasije.flowershop.backend.model.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByIsDeletedFalse();
 }
