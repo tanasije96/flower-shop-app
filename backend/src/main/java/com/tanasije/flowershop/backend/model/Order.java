d3e48fc009e96d6906013889bbc697d30f7ad980
tanasije96 <marko.tanasije@gmail.com>
2026-06-01 23:11:47 -0400

    created model for order

---
+++ backend/src/main/java/com/tanasije/flowershop/backend/model/Order.java
@@ -0,0 +1,32 @@
+package com.tanasije.flowershop.backend.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "orders") 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal totalPrice;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+    private LocalDateTime createdAt;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderItem> items;
+}
