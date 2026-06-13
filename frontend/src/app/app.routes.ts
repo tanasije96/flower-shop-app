add82d225fafe0f5086f866c9de136e0f0b5adfe
tanasije96 <marko.tanasije@gmail.com>
2026-06-13 14:11:50 -0400

    Added route for product list componenet

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ProductListComponent } from './components/product-list/product-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: ProductListComponent }
+];
