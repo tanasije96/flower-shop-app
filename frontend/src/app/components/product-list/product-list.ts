004c7e1dd9ea538576eb16f1d5c85d5ae0fda871
tanasije96 <marko.tanasije@gmail.com>
2026-06-13 14:13:17 -0400

    Created product list component

---
+++ frontend/src/app/components/product-list/product-list.ts
@@ -0,0 +1,50 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { Product } from '../../models/product';
+import { ProductService } from '../../services/product.service';
+import { MatTableModule } from '@angular/material/table';
+
+@Component({
+  selector: 'app-product-list',
+  imports: [CommonModule, MatTableModule],
+  templateUrl: './product-list.html',
+  styleUrl: './product-list.css',
+})
+export class ProductListComponent implements OnInit {
+  products: Product[] = [];
+  displayedColumns: string[] = ['name', 'price', 'type'];
+  loading = true;
+  error = '';
+
+  constructor(
+    private productService: ProductService,
+    private cd: ChangeDetectorRef
+  ) {}
+
+  ngOnInit(): void {
+    this.loadProducts();
+  }
+
+  loadProducts(): void {
+    console.log('Calling backend...');
+
+    this.productService.getAllProducts().subscribe({
+      next: (data) => {
+        console.log('DATA RECEIVED:', data);
+
+        this.products = data;
+        this.loading = false;
+
+        this.cd.detectChanges();
+      },
+      error: (err) => {
+        console.log('ERROR:', err);
+
+        this.error = 'Failed to load products';
+        this.loading = false;
+
+        this.cd.detectChanges();
+      }
+    });
+  }
+}
