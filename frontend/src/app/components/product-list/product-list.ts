5a4b3991c18d80fb3d6b3e93e8de2b79720d8388
tanasije96 <marko.tanasije@gmail.com>
2026-06-20 10:38:54 -0400

    Added buy button to product list

--- frontend/src/app/components/product-list/product-list.ts
+++ frontend/src/app/components/product-list/product-list.ts
@@ -5,22 +5,27 @@ import { ProductService } from '../../services/product.service';
 import { MatTableModule } from '@angular/material/table';
 import { getProductTypeDisplay } from '../../utils/product-utils';
 import { ProductType } from '../../models/product-type';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-product-list',
-  imports: [CommonModule, MatTableModule],
+  imports: [CommonModule, MatTableModule, MatSnackBarModule, MatButtonModule,
+    MatIconModule],
   templateUrl: './product-list.html',
   styleUrl: './product-list.css',
 })
 export class ProductListComponent implements OnInit {
   products: Product[] = [];
-  displayedColumns: string[] = ['name', 'price', 'type'];
+  displayedColumns: string[] = ['name', 'price', 'type', 'actions'];
   loading = true;
   error = '';
 
   constructor(
     private productService: ProductService,
-    private cd: ChangeDetectorRef
+    private cd: ChangeDetectorRef,
+    private snackBar: MatSnackBar
   ) {}
 
   ngOnInit(): void {
@@ -51,5 +56,13 @@ export class ProductListComponent implements OnInit {
   }
   getDisplayType(type: ProductType): string {
     return getProductTypeDisplay(type);
+  }
+
+  buyProduct(product: Product): void {
+    this.snackBar.open(
+      `You've successfully purchased ${product.name}!`,
+      'Close',
+      { duration: 3000 }
+    );
   }
 }
